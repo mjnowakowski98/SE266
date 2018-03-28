5e251d745f522e0a6e13a1059d81c8ef725fab88
Nowakowski <mjnowakowski@labs.neit.edu>
2018-03-28 10:48:45 -0400

    Color grid final

--- Assignment1/index.php
+++ Assignment1/index.php
@@ -1,35 +1,40 @@
 <?php
-	$doc = new DOMDocument('1.0', 'UTF-8');
+	$doc = new DOMDocument('1.0', 'UTF-8'); // Create new document root/body
+	
+	// Create base table elements
 	$table = $doc->createElement('table');
 	$tableBody = $doc->createElement('tbody');
 
 	// Generate table rows
 	for($i = 0; $i < 10; $i++) {
-		$newRow = $doc->createElement('tr');
+		$newRow = $doc->createElement('tr'); // Create new row
 
 		// Generate table columns
 		for($j = 0; $j < 10; $j++) {
+			// Generate/combine RGB values
 			$red = dechex(mt_rand(00,255));
 			$green = dechex(mt_rand(0,255));
 			$blue = dechex(mt_rand(0,255));
 			$color = "#$red$green$blue";
 			
+			// Create new column in row
 			$newCol = $doc->createElement('td');
-			$newCol->setAttribute('style',"background-color:$color;");
-			$newCol->appendChild($doc->createTextNode($color));
-			$newCol->appendChild($doc->createElement('br'));
+			$newCol->setAttribute('style',"background-color:$color;"); // Change css color
+			$newCol->appendChild($doc->createTextNode($color)); // Output text in black
+			$newCol->appendChild($doc->createElement('br')); // Add line break
 
-			$newSpan = $doc->createElement('span');
+			$newSpan = $doc->createElement('span'); // Create new span
 			$newSpan->setAttribute('style',"color:ffffff;");
-			$newSpan->appendChild($doc->createTextNode($color));
-			
+			$newSpan->appendChild($doc->createTextNode($color)); // To output text in white
+
+			// Append span/col to row
 			$newCol->appendChild($newSpan);
 			$newRow->appendChild($newCol);
 		}
-		$tableBody->appendChild($newRow);
+		$tableBody->appendChild($newRow); // append row to tbody
 	}
 
-	// Append elements to DOM
+	// Append elements to DOM body
 	$table->appendChild($tableBody);
 	$doc->appendChild($table);
 	echo $doc->saveXML();
