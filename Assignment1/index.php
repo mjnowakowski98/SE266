11ccfb8a321c1e313ba0c40015ab80222019ce74
Nowakowski <mjnowakowski@labs.neit.edu>
2018-03-28 10:24:10 -0400

    Setup Table

--- Assignment1/index.php
+++ Assignment1/index.php
@@ -1,12 +1,28 @@
-<!DOCTYPE html>
-<html>
-	<head>
-		<meta charset="UTF-8">
-		<title>SE266 Nowakowski L1</title>
-	</head>
-	<body>
-		<?php
-			$elem_table = new 
-		?>
-	</body>
-</html>
+<?php header("Content-type:text/css");
+	
+?>
+
+<?php
+	$doc = new DOMDocument('1.0', 'UTF-8');
+	$table = $doc->createElement('table');
+	$tableBody = $doc->createElement('tbody');
+
+	for($i = 0; $i < 10; $i++) {
+		$newRow = $doc->createElement('tr');
+		for($j = 0; $j < 10; $j++) {
+			$red = dechex(mt_rand(0,255));
+			$green = dechex(mt_rand(0,255));
+			$blue = dechex(mt_rand(0,255));
+			$color = "$red$green$blue";
+			
+			$newCol = $doc->createElement('td');
+			$newCol->appendChild($doc->createTextNode($color));
+			$newRow->appendChild($newCol);
+		}
+		$tableBody->appendChild($newRow);
+	}
+
+	$table->appendChild($tableBody);
+	$doc->appendChild($table);
+	echo $doc->saveXML();
+?>
