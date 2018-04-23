55ce2ec1de5f5bdff296343d51304eaef3328085
Nowakowski <mjnowakowski@labs.neit.edu>
2018-04-23 09:17:42 -0400

    Setup basic form entry

--- lab5/index.php
+++ lab5/index.php
@@ -1,3 +1,9 @@
+<?php
+    if(array_key_exists('crawlUrl', $_REQUEST))
+        $strInput =  $_REQUEST['crawlUrl'];
+    else $strInput = NULL;
+?>
+
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -5,13 +11,23 @@
         <meta charset="UTF-8">
     </head>
     <body>
-        <header>
-            <h1>Sites Application</h1>
-        </header>
-
         <div id="wrapper">
+            <nav>
+                <a href="./index.php">Site Entry</a>
+                <a href="./list.php">Site Listing</a>
+            </nav>
+
+            <header>
+                <h1>Sites Application</h1>
+            </header>
+
+            <?php
+                if(!preg_match('/(https?:\/\/[\da-z\.-]+\.[a-z\.]{2,6}[\/\w \.-]+)/', $strInput))
+                    echo 'Please enter a valid URL';
+            ?>
+
             <form action="index.php" method="GET">
-                <input type="text" name="crawlUrl">
+                <input type="text" name="crawlUrl" value="<?php echo $strInput?>">
                 <input type="submit">
             </form>
         </div>
