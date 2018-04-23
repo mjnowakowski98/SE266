fbfdc7b92bb51dd23ce92ea3a3142a0aa2ed19a4
Nowakowski <mjnowakowski@labs.neit.edu>
2018-04-23 08:48:13 -0400

    Setup initial layout

---
+++ lab5/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Lab 5</title>
+        <meta charset="UTF-8">
+    </head>
+    <body>
+        <header>
+            <h1>Sites Application</h1>
+        </header>
+
+        <div id="wrapper">
+            <form action="index.php" method="GET">
+                <input type="text" name="crawlUrl">
+                <input type="submit">
+            </form>
+        </div>
+    </body>
+</html>
