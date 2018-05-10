020c2800c0cf26afff9d60162b54600e8dc3b7e3
Matthew Nowakowski <mjnowakowski@email.neit.edu>
2018-05-10 18:22:01 -0400

    More changes

--- lab6/common/forms/signup.php
+++ lab6/common/forms/signup.php
@@ -1,23 +1,19 @@
-<?php
-    if(session_name() == 'genericSession') {
-        $mode = 'anonymous';
-    }
-?>
-
-
 <div id="dimmer"></div>
 
 <section id="signupForm" class="dimmerOverlay">
     <form action="#" method="POST">
-        <label>User ID: <input class="txtBox" type="text" name="uName"></label>
+        <h3 class="formCenter">Sign Up</h3>
+
+        <hr>
+        <label>Email: <input class="txtBox" type="text" name="email"></label>
+        <label>User ID: <input class="txtBox" type="text" name="usrName"></label>
         <label>Password: <input class="txtBox" type="text" name="pass"></label>
-        <label>Password (repeat): <input class="txtBox" type="text" name="p2"></label>
+        <label>Password (repeat): <input class="txtBox" type="text" name="pass2"></label>
         <label>First name: <input class="txtBox" type="text" name="fName"></label>
         <label>Last name: <input class="txtBox" type="text" name="lName"></label>
-        <label>Email: <input class="txtBox" type="text" name="email"></label>
         <p class="formCenter">Administrator:</p>
         <div class="formCenter">
-            <label><input type="radio" name="isAdmin" value="false"> No</label>
+            <label><input type="radio" name="isAdmin" value="false" checked> No</label>
             <label><input type="radio" name="isAdmin" value="true"> Yes</label>
         </div>
 
